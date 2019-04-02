c029268a9e964519e7e3145de930bc645c6d203d
Oleg Zenkin <oleg.zenkin@stackpath.com>
2019-04-02 09:47:06 +0300

    Logging catalogs loading

--- app/Http/Middleware/ExternalCatalogs.php
+++ app/Http/Middleware/ExternalCatalogs.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use Log;
 use Closure;
 use Illuminate\Support\Facades\Cache;
 
@@ -17,25 +18,28 @@ class ExternalCatalogs
      */
     public function handle($request, Closure $next)
     {
-        if (!Cache::has('country_codes', 0)) {
+        if (!Cache::has('country_codes')) {
             try {
                 $arr = json_decode(
                     file_get_contents('https://api.hostaway.com/countries'),
                     true
                 );
-                Cache::put('country_codes', $arr['result']);
+                Cache::put('country_codes', $arr['result'], env('CACHE_EXPIRE'));
+				Log::info('Cache: `country_codes` updated');
             } catch (\Exception $e) {
                 exit('Something went wrong. '.$e->getMessage());
             }
         }
-        if (!Cache::has('timezones', 0)) {
+        if (!Cache::has('timezones')) {
             try {
                 $arr = json_decode(
                     file_get_contents('https://api.hostaway.com/timezones'),
                     true
                 );
-                Cache::put('timezones', $arr['result']);
+                Cache::put('timezones', $arr['result'], env('CACHE_EXPIRE'));
+				Log::info('Cache: `timezones` updated');
             } catch (\Exception $e) {
+				Log::error('Cache: `timezones` update error. '.$e->getMessage());
                 exit('Something went wrong. '.$e->getMessage());
             }
         }
